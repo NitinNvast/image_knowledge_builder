d240371d5d7d9f7a1f60564eeb726b4a700f6aca
nitinn@valueaddsofttech.com <nitinn@valueaddsofttech.com>
2025-05-08 11:31:25 +0530

    fourth commit.

--- src/components/learn.rs
+++ src/components/learn.rs
@@ -9,61 +9,62 @@ pub fn LearnSection(props: LearnSectionProps) -> Element {
     rsx! {
         section { class: "w-full flex flex-col gap-2 p-4 text-sm",
 
-           div { class: "flex flex-col gap-2 h-32",
+            div { class: "flex flex-col gap-2 h-32",
 
-               div { class: "flex flex-wrap gap-x-10 justify-start items-start",
+                div { class: "flex flex-wrap gap-x-10 justify-start items-start",
 
-                   div { class: "flex justify-start items-center gap-2",
-                       button { class: "bg-white p-3 rounded border shadow", "Learn" }
-                   }
+                    div { class: "flex justify-start items-center gap-2",
+                        button { class: "bg-white p-3 rounded border shadow", "Learn" }
+                    }
 
-                   div { class: "flex flex-col gap-2",
-                       label { "Learn Mode" }
-                       select { class: "border rounded",
-                           option { "Codebook" }
-                           option { "Annotations" }
-                       }
-                   }
+                    div { class: "flex flex-col gap-2",
+                        label { "Learn Mode" }
+                        select { class: "border rounded",
+                            option { "Codebook" }
+                            option { "Annotations" }
+                        }
+                    }
 
-                   div { class: "flex flex-col gap-2",
-                       label { "Categories" }
-                       select { class: "border rounded",
-                           option { "Increment" }
-                           option { "Constant" }
-                           option { "MaxDeltaAmplitude" }
-                           option { "AvgAmplitude" }
-                           option { "InputIndex" }
-                       }
-                   }
+                    div { class: "flex flex-col gap-2",
+                        label { "Categories" }
+                        select { class: "border rounded",
+                            option { "Increment" }
+                            option { "Constant" }
+                            option { "MaxDeltaAmplitude" }
+                            option { "AvgAmplitude" }
+                            option { "InputIndex" }
+                        }
+                    }
+                }
 
-               }
+                div { class: "flex flex-wrap gap-10 justify-start items-center",
 
-               div { class: "flex flex-wrap gap-10 justify-start items-center",
+                    div { class: "flex justify-start items-center gap-2",
+                        input { r#type: "checkbox", id: "auto" }
+                        label { r#for: "auto", "Auto" }
+                    }
 
-                   div { class: "flex justify-start items-center gap-2",
-                       input { r#type: "checkbox", id: "auto" }
-                       label { r#for: "auto", "Auto" }
-                   }
+                    div { class: "flex justify-start items-center gap-2",
+                        button { class: "co bg-white px-3 py-1 rounded border shadow",
+                            "Select full image"
+                        }
+                    }
 
-                   div { class: "flex justify-start items-center gap-2",
-                       button { class: "co bg-white px-3 py-1 rounded border shadow", "Select full image" }
-                   }
-
-                   div { class: "flex justify-start items-  gap-2 -ml-5",
-                       label { "StepXY" }
-                       input {
-                           r#type: "number",
-                           value: "16",
-                           class: "w-12 p-1 border rounded",
-                       }
-                       input {
-                           r#type: "number",
-                           value: "16",
-                           class: "w-12 p-1 border rounded",
-                       }
-                   }
-               }
-           }
+                    div { class: "flex justify-start items-  gap-2 -ml-5",
+                        label { "StepXY" }
+                        input {
+                            r#type: "number",
+                            value: "16",
+                            class: "w-12 p-1 border rounded",
+                        }
+                        input {
+                            r#type: "number",
+                            value: "16",
+                            class: "w-12 p-1 border rounded",
+                        }
+                    }
+                }
+            }
 
 
             // Image preview placeholder
